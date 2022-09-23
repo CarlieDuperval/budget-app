c3f90c45033a80d6610a905e4cf4e0c15c16f3a3
Carlie Duperval <ritchellcarlie@gmail.com>
2022-09-23 01:58:34 -0400

    Added component Buttons AddBudget and AddExpense

--- src/components/AddBudget.jsx
+++ src/components/AddBudget.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import { Container, Stack, Button } from "react-bootstrap";
 
-
-
-
-
 const AddBudget = () => {
-    return (
-        <Container>
-            <Stack direction="horizontal" gap="2" className="mb-4">
-                <h1 className="me-auto">Budgets</h1>
-                <Button></Button>
-
-            </Stack>
-
-        
-        
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+    </Container>
+  );
+};
+export default AddBudget;
