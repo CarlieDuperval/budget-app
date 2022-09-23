77e8816deca15dc59a7bdb675ee9e350f547da16
Carlie Duperval <ritchellcarlie@gmail.com>
2022-09-23 08:22:14 -0400

    Added Budget Components Props

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
-const BudgetCard = (name)=> {
+const BudgetCard = (name, amount, max)=> {
   return (
     <Card>
         <Card.Body>
             <Card.Title>
                 <div>{name}</div>
+                <div>{amount} / {max}</div>
             </Card.Title>
         </Card.Body>
     </Card>
   )
 }
+export default BudgetCard;
