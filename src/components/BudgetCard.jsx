4d1b839e8ffe4cd6a35c76206bd575f10f1af2b5
Carlie Duperval <ritchellcarlie@gmail.com>
2022-09-23 18:41:56 -0400

    Added BudgetCard.css

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { currencyFormatter } from './Tools'
+import './BudgetCard.css'
 
 const BudgetCard = ({name, amount, max}) => {
   return (
     <Card>
         <Card.Body>
-            <Card.Title>
+            <Card.Title className='card-title'>
                 <div>{name}</div>
                 <div>
                   {currencyFormatter.format(amount)}/ {currencyFormatter.format(max)}
