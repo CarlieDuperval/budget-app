88aed28dc6884b371f447a7c72b6e06fdbe1ecdd
Carlie Duperval <ritchellcarlie@gmail.com>
2022-09-24 08:48:35 -0400

    Updated LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import AddBudget from './components/AddBudget';
 // import AddBudgetModal from './components/AddBudgetModal';
 
 
 const App = () => {
-  // const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
 
   return( 
-    <>
   < AddBudget />
-  {/* <AddBudgetModal show={showAddBudgetModal} handleClose={()=> setShowAddBudgetModal(false) }/> */}
-  </>
   )
 }
 
--- src/hooks/LocalStorage.jsx
+++ src/hooks/LocalStorage.jsx
@@ -13,7 +13,7 @@ const LocalStorage = (key , defaultValue) => {
     })
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value))
-    }, [key, value])
+    }, [key, value]) // when ever a value change , this value will update in the localStorage
 
     return [value, setValue]
  
