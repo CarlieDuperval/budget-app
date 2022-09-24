a40494a95438ac7f3bc6d98106b821611870b177
Carlie Duperval <ritchellcarlie@gmail.com>
2022-09-23 20:42:57 -0400

    Styled BudgetCard with bootstrap and css

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -8,9 +8,12 @@ const BudgetCard = ({name, amount, max}) => {
     <Card>
         <Card.Body>
             <Card.Title className='card-title'>
-                <div>{name}</div>
-                <div>
-                  {currencyFormatter.format(amount)}/ {currencyFormatter.format(max)}
+                <div className='me-2'>{name}</div>
+                <div className='amount-prop'>
+                  {currencyFormatter.format(amount)}
+                  <span className='text-muted fs-6 ms-1'>
+                  / {currencyFormatter.format(max)}
+                  </span>
                   </div>
             </Card.Title>
         </Card.Body>
