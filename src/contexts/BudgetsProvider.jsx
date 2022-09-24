1f228acd3383e24b971c74ff69def95ab88bf42c
Carlie Duperval <ritchellcarlie@gmail.com>
2022-09-24 00:09:25 -0400

    Added hooks folder and LocalStorage file

--- src/contexts/BudgetsProvider.jsx
+++ src/contexts/BudgetsProvider.jsx
@@ -1,9 +1,66 @@
 import React from "react"
+// import { useState } from "react"
+import { useContext } from "react"
+import { v4 as uuidV4 } from "uuid" // to give a unique id to each budget 
+import LocalStorage from "../hooks/LocalStorage"
+
+const BudgetsContext = React.createContext()
+
 
 export const useBudgets = () => {
+    return useContext(BudgetsContext)
 
 }
 
 export const BudgetsProvider = ({children }) => {
-    return children
+    const [budgets, setBudgets] = LocalStorage('budgets',[])
+    const [expenses, setExpenses] = LocalStorage('expenses',[])
+
+
+    const getBudgetExpenses = (budgetId) => {
+        return expenses.filter(expense => expense.budgetId === budgetId)
+    }
+    const addBudget = (name, max) => {
+        setBudgets(previewBudgets => {
+            if(previewBudgets.find(budget => budget.name === name)){
+                return previewBudgets
+            }
+            return [...previewBudgets, {id : uuidV4(), name, max }]
+        })
+        
+    }
+    const addExpense = ({description, amount, budgetId}) => {
+        setExpenses(previewExpenses => {
+            return [...previewExpenses, {id : uuidV4(), description, amount, budgetId}]
+        })
+        
+    } 
+    const deleteBudget = ({id}) => {
+        setBudgets(previewBudgets => {
+            return previewBudgets.filter(budget => budget.id !== id)
+
+        })
+    }
+    const deleteExpense = ({id}) => {
+        setExpenses(previewExpenses => {
+            return previewExpenses.filter(expense => expense.id !== id)
+
+        })
+
+    }
+
+    return (
+    < BudgetsContext.Provider value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense
+
+    }}
+    > {children}
+        </BudgetsContext.Provider>
+        )
 }
