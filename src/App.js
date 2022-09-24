5e06bc39674a40a8427ad3f4b7c8ad09f432f696
Carlie Duperval <ritchellcarlie@gmail.com>
2022-09-24 07:33:57 -0400

    Added Modal and form form on AddBudgetModal component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import AddBudget from './components/AddBudget';
+import AddBudgetModal from './components/AddBudgetModal';
 
 
 const App = () => {
   return( 
     <>
   < AddBudget />
+  <AddBudgetModal show/>
   </>
   )
 }
