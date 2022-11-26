21e75f7e61afef58182e3e990ab1d5815fe69cd3
Carlie Duperval <ritchellcarlie@gmail.com>
2022-11-26 18:06:05 -0500

    Updated components

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,56 +1,65 @@
-import React from 'react'
-import { Button, Card, ProgressBar, Stack } from 'react-bootstrap'
-import { currencyFormatter } from './Tools'
-import './BudgetCard.css'
-import getProgressBarVariant from './ProgressVariant'
+import React from "react";
+import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
+import { currencyFormatter } from "./Tools";
+import "./BudgetCard.css";
+import getProgressBarVariant from "./ProgressVariant";
 
-const BudgetCard = ({name, amount, max, gray, hideButtons, onAddExpenseClick, onViewExpensesClick }) => {
-
-  const refColor = []
-     if (amount > max){
-      refColor.push('bg-danger', 'bg-opacity-10')
-    } else if (gray){
-      refColor.push("bg-light")
-    }
+const BudgetCard = ({
+  name,
+  amount,
+  max,
+  gray,
+  hideButtons,
+  onAddExpenseClick,
+  onViewExpensesClick,
+}) => {
+  const refColor = [];
+  if (amount > max) {
+    refColor.push("bg-danger", "bg-opacity-10");
+  } else if (gray) {
+    refColor.push("bg-light");
+  }
   return (
-    <Card className={refColor.join(' ')}>
-        <Card.Body>
-            <Card.Title className='card-title'>
-                <div className='me-2'>{name}</div>
-                <div className='amount-prop'>
-                  {currencyFormatter.format(amount)}
-                  {max && (
-                  <span className='text-muted fs-6 ms-1'>
-                  / {currencyFormatter.format(max)}
-                  </span>
-                  )}
-                  </div>
-            </Card.Title>
-            {max && ( <ProgressBar 
-            className="rounded-pill" 
+    <Card className={refColor.join(" ")}>
+      <Card.Body>
+        <Card.Title className="card-title">
+          <div className="me-2">{name}</div>
+          <div className="amount-prop">
+            {currencyFormatter.format(amount)}
+            {max && (
+              <span className="text-muted fs-6 ms-1">
+                / {currencyFormatter.format(max)}
+              </span>
+            )}
+          </div>
+        </Card.Title>
+        {max && (
+          <ProgressBar
+            className="rounded-pill"
             variant={getProgressBarVariant(amount, max)}
-            min={0} 
+            min={0}
             max={max}
             now={amount}
-            />
-            )}
-            {!hideButtons && (
-            <Stack className='mt-4' direction='horizontal' gap='2' >
-              <Button 
-              variant='outline-primary' 
-              className='ms-auto'
+          />
+        )}
+        {!hideButtons && (
+          <Stack className="mt-4" direction="horizontal" gap="2">
+            <Button
+              variant="outline-primary"
+              className="ms-auto"
               onClick={onAddExpenseClick}
-              >
-                Add Expense
-              </Button>
-              <Button onClick={onViewExpensesClick}
-              variant='outline-secondary'> View Expenses </Button>
-            </Stack>
-            )}
-        </Card.Body>
+            >
+              Add Expense
+            </Button>
+            <Button onClick={onViewExpensesClick} variant="outline-secondary">
+              {" "}
+              View My Expenses{" "}
+            </Button>
+          </Stack>
+        )}
+      </Card.Body>
     </Card>
-  )
-}
-
+  );
+};
 
 export default BudgetCard;
--- src/contexts/BudgetsProvider.jsx
+++ src/contexts/BudgetsProvider.jsx
@@ -1,74 +1,71 @@
-import React from "react"
-// import { useState } from "react"
-import { useContext } from "react"
-import { v4 as uuidV4 } from "uuid" // to give a unique id to each budget 
-import LocalStorage from "../hooks/LocalStorage"
+import React from "react";
+import { useContext } from "react";
+import { v4 as uuidV4 } from "uuid"; // to give a unique id to each budget
+import LocalStorage from "../hooks/LocalStorage";
 
-const BudgetsContext = React.createContext()
+const BudgetsContext = React.createContext();
 
-export const UNCATEGORIZED_BUDGET_ID = "Uncategorized"
+export const UNCATEGORIZED_BUDGET_ID = "Uncategorized";
 
 export const useBudgets = () => {
-    return useContext(BudgetsContext)
+  return useContext(BudgetsContext);
+};
 
-}
+export const BudgetsProvider = ({ children }) => {
+  const [budgets, setBudgets] = LocalStorage("budgets", []);
+  const [expenses, setExpenses] = LocalStorage("expenses", []);
 
-export const BudgetsProvider = ({children }) => {
-    const [budgets, setBudgets] = LocalStorage('budgets',[])
-    const [expenses, setExpenses] = LocalStorage('expenses',[])
+  const getBudgetExpenses = (budgetId) => {
+    return expenses.filter((expense) => expense.budgetId === budgetId);
+  };
+  const addBudget = ({ name, max }) => {
+    setBudgets((previewBudgets) => {
+      // if the budget has the same name as a preview one
+      if (previewBudgets.find((budget) => budget.name === name)) {
+        return previewBudgets;
+      }
+      return [...previewBudgets, { id: uuidV4(), name, max }];
+    });
+  };
+  const addExpense = ({ description, amount, budgetId }) => {
+    setExpenses((previewExpenses) => {
+      return [
+        ...previewExpenses,
+        { id: uuidV4(), description, amount, budgetId },
+      ];
+    });
+  };
+  const deleteBudget = ({ id }) => {
+    setExpenses((previewExpenses) => {
+      return previewExpenses.map((expense) => {
+        if (expense.budgetId !== id) return expense;
+        return { ...expense, budgetId: UNCATEGORIZED_BUDGET_ID };
+      });
+    });
+    setBudgets((previewBudgets) => {
+      return previewBudgets.filter((budget) => budget.id !== id);
+    });
+  };
+  const deleteExpense = ({ id }) => {
+    setExpenses((previewExpenses) => {
+      return previewExpenses.filter((expense) => expense.id !== id);
+    });
+  };
 
-
-    const getBudgetExpenses = (budgetId) => {
-        return expenses.filter(expense => expense.budgetId === budgetId)
-    }
-const addBudget = ({name, max}) => {
-        setBudgets(previewBudgets => {
-            // if the budget has the same name as a preview one 
-            if(previewBudgets.find(budget => budget.name === name)){
-                return previewBudgets
-            }
-            return [...previewBudgets, {id : uuidV4(), name, max }]
-        })
-        
-    }
-    const addExpense = ({description, amount, budgetId}) => {
-        setExpenses(previewExpenses => {
-            return [...previewExpenses, {id : uuidV4(), description, amount, budgetId}]
-        })
-        
-    } 
-    const deleteBudget = ({id}) => {
-        setExpenses(previewExpenses => {
-            return previewExpenses.map(expense =>{
-                if (expense.budgetId !== id) return expense
-                return {...expense, budgetId: UNCATEGORIZED_BUDGET_ID}
-            })
-        })
-        setBudgets(previewBudgets => {
-            return previewBudgets.filter(budget => budget.id !== id)
-
-        })
-    }
-    const deleteExpense = ({id}) => {
-        setExpenses(previewExpenses => {
-            return previewExpenses.filter(expense => expense.id !== id)
-
-        })
-
-    }
-
-    return (
-    < BudgetsContext.Provider value={{
+  return (
+    <BudgetsContext.Provider
+      value={{
         budgets,
         expenses,
         getBudgetExpenses,
         addExpense,
         addBudget,
         deleteBudget,
-        deleteExpense
-
-    }}
-    > {children}
-        </BudgetsContext.Provider>
-        )
-}
+        deleteExpense,
+      }}
+    >
+      {" "}
+      {children}
+    </BudgetsContext.Provider>
+  );
+};
